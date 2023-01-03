9e9f3a168e204d4389c67bd311217fcde6bbbc1d
Gagan Tunugntla <gugzkumar@gmail.com>
2023-01-02 19:40:53 -0500

    Remove unecessary logic for cms content

--- buildCMSContent.js
+++ buildCMSContent.js
@@ -2,8 +2,7 @@ const path = require('path');
 const yaml = require('js-yaml');
 var fs = require('fs');
 
-const OUTPUT_DIRECTORY = 'public/content';
-const INPUT_CMS_DIRECTORY = 'cmsContent';
+const INPUT_CMS_DIRECTORY = 'public/content';
 const OUTPUT_CMS_CONFIGS_DIRECTORY = 'src/cmsConfigs';
 
 // A function that reads a yaml file and returns the content as a JSON object using js-yaml
@@ -76,31 +75,26 @@ const main = () => {
     const { collections } = readYamlFile(path.join(__dirname, 'public/admin/config.yml'));
     const inputFolders = collections.map(collection => collection.name);
 
-    // Deletes a files in public/content and src/cmsConfigs
-    deleteFolderRecursive(path.join(__dirname, `/${OUTPUT_DIRECTORY}`));
+    // Deletes a files in src/cmsConfigs
     deleteFolderRecursive(path.join(__dirname, `/${OUTPUT_CMS_CONFIGS_DIRECTORY}`));
 
-    const outputFiles = [];
+    const jsonFiles = [];
     // Loop through all folders in INPUT_FOLDERS
     inputFolders.forEach(folder => {
         // Read all files in the folder
         const files = fs.readdirSync(path.join(__dirname, INPUT_CMS_DIRECTORY, folder));
-        const outputFolder = path.join(__dirname, OUTPUT_DIRECTORY, folder);
-        fs.mkdirSync(outputFolder, { recursive: true });
-        // Loop through all files in the folder and copy them to public/content
+        // Loop through all files in the folder
         files.forEach(file => {
             const filePath = path.join(__dirname, INPUT_CMS_DIRECTORY, folder, file);
-            const newFilePath = path.join(outputFolder, `${file}`);
-            copyFile(filePath, newFilePath);
             const cmsFileAsJSON = readJSONFile(filePath);
             // Remove body from the JSON object to reduce the size of the config file
             delete cmsFileAsJSON['body'];
             cmsFileAsJSON['routePath'] = `${folder}/${path.parse(file).name}`;
-            outputFiles.push(cmsFileAsJSON);
+            jsonFiles.push(cmsFileAsJSON);
         });
         // Write the JSON object to a file in src/cmsConfigs containing metadata for all files in the folder
         const postConfigFilePath = path.join(__dirname, OUTPUT_CMS_CONFIGS_DIRECTORY, `${folder}.json`);
-        writeJSONToFile(postConfigFilePath, outputFiles);
+        writeJSONToFile(postConfigFilePath, jsonFiles);
     });
 }
 
